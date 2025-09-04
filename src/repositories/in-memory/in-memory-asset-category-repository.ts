bec6d252820c0e7fa8627d8fd07fe8ec8b65e56f
Marcelo Costa <marcelodscosta@yahoo.com.br>
2025-09-04 20:11:42 -0300

    feat: create InMemoryAssetCategoryRepository

---
+++ src/repositories/in-memory/in-memory-asset-category-repository.ts
@@ -0,0 +1,24 @@
+import { randomUUID } from 'crypto'
+import { Prisma, AssetCategory } from '../../generated/prisma'
+import { IAssetCategoryRepository } from '../interfaces/IAssetCategoryRepository'
+
+export class inMemoryAssetCategoryRepository
+  implements IAssetCategoryRepository
+{
+  public items: AssetCategory[] = []
+
+  async create(data: Prisma.AssetCategoryCreateInput): Promise<AssetCategory> {
+    const assetCategory = {
+      id: data.id ?? randomUUID(),
+      name: data.name,
+      description: data.description ?? null,
+      type: data.type,
+      created_at: new Date(),
+      updated_at: data.updated_at as Date,
+      is_Active: data.is_Active ?? false,
+    }
+    this.items.push(assetCategory)
+
+    return assetCategory
+  }
+}
