9110b659b6dd98ad4882decbfdf5970cdb526eaf
Marcelo Costa <marcelodscosta@yahoo.com.br>
2025-09-20 10:38:42 -0300

    feat: define interfaces for asset entities

--- src/repositories/interfaces/IAssetRepository.ts
+++ src/repositories/interfaces/IAssetRepository.ts
@@ -2,4 +2,10 @@ import { Asset, Prisma } from '../../generated/prisma'
 
 export interface IAssetRepository {
   create(data: Prisma.AssetUncheckedCreateInput): Promise<Asset>
+  findById(id: string): Promise<Asset | null>
+  // searchAssets(query: string, page: number): Promise<Asset[]>
+  // updateAsset(id: string, data: Prisma.AssetUpdateInput): Promise<Asset>
+
+  // findByPlate(plate: string): Promise<Asset | null>
+  // findBySerialNumber(serialNumber: string): Promise<Asset | null>
 }
