8f69b28c20a9a523202285c4287235eba7922782
Marcelo Costa <marcelodscosta@yahoo.com.br>
2025-12-21 21:36:41 -0300

    refactor: rename make factory to make-find-all-supplier-unpaginated

--- src/services/factories/make-find-all-supplier-unpaginated.ts
+++ src/services/factories/make-find-all-supplier-unpaginated.ts
@@ -1,5 +1,5 @@
 import { PrismaSupplierRepository } from '../../repositories/prisma/prisma-supplier-repository'
-import { FindAllUnpaginatedSupplierUseCase } from '../supplier/find-all-unpaginated-use-case'
+import { FindAllUnpaginatedSupplierUseCase } from '../supplier/find-all-supplier-unpaginated-use-case'
 
 export function makeFindAllUnpaginatedSupplier() {
   const assetRepository = new PrismaSupplierRepository()
