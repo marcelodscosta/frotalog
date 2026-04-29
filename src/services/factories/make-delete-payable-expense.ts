77f3403d96f6141f0a38859019982c0d54f88257
Marcelo Costa <marcelodscosta@yahoo.com.br>
2026-04-29 15:28:51 -0300

    refactor: fix import paths for payable expense factory dependencies

--- src/services/factories/make-delete-payable-expense.ts
+++ src/services/factories/make-delete-payable-expense.ts
@@ -1,7 +1,7 @@
-import { PrismaPayableExpenseRepository } from '../repositories/prisma/prisma-payable-expense-repository'
-import { PrismaBankAccountRepository } from '../repositories/prisma/prisma-bank-account-repository'
-import { PrismaFinancialTransactionRepository } from '../repositories/prisma/prisma-financial-transaction-repository'
-import { DeletePayableExpenseUseCase } from './accounts-payable/delete-expense-use-case'
+import { PrismaPayableExpenseRepository } from '../../repositories/prisma/prisma-payable-expense-repository'
+import { PrismaBankAccountRepository } from '../../repositories/prisma/prisma-bank-account-repository'
+import { PrismaFinancialTransactionRepository } from '../../repositories/prisma/prisma-financial-transaction-repository'
+import { DeletePayableExpenseUseCase } from '../accounts-payable/delete-expense-use-case'
 
 export function makeDeletePayableExpense() {
   const payableExpenseRepository = new PrismaPayableExpenseRepository()
